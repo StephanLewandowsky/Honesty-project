3de83dc089869cd2c2ca58ee1452866f0ec8a6fb
Jana <jana.lasser@ds.mpg.de>
2022-03-30 16:28:50 +0200

    added missing imports

--- code/corpus_twitter/get_conversation_ids_for_csv_parsing.py
+++ code/corpus_twitter/get_conversation_ids_for_csv_parsing.py
@@ -1,9 +1,11 @@
 from os.path import join
+from os import listdir
+import numpy as np
 
-json_ids = os.listdir("/data/honesty/corpora/Twitter/US_politician_twitter_conversations")
+json_ids = listdir("/data/honesty/corpora/Twitter/US_politician_twitter_conversations")
 json_ids = set([i.split(".")[0] for i in json_ids])
 
-csv_ids = os.listdir("/data/honesty/corpora/Twitter/US_politician_twitter_conversations_csv")
+csv_ids = listdir("/data/honesty/corpora/Twitter/US_politician_twitter_conversations_csv")
 csv_ids = set([i.split(".")[0] for i in csv_ids])
 print(f"{len(json_ids)} json conversations, {len(csv_ids)} csv conversations")
 
